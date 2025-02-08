0b39b0a7323d5156ec50fd34c92f5a73114a037f
zakerous <m.z.humayoon@gmail.com>
2025-02-08 14:54:10 +0300

    Navbar: done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import Navbar from "./components/Navbar";
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-white selection:text-black">
+      <div className="fixed top-0 -z-10 h-full w-full">
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#101010_1px)] bg-[size:20px_20px]"></div>
+      </div>
+
+      <div className="container mx-auto px-2">
+        <Navbar></Navbar>
+      </div>
+    </div>
+  );
 };
 
 export default App;
