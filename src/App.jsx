a27c7d8b55aac4317c128ff64b685096a8c65a0c
zakerous <m.z.humayoon@gmail.com>
2025-02-11 21:38:05 +0300

    minor padding and margin changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
         <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(250,119,200,0.3),rgba(255,255,255,0))]"></div>
       </div>
 
-      <div className="container mx-auto lg:pl-8">
+      <div className="container mx-auto lg:pl-8 px-4">
         <Navbar />
         <div className="main lg:mx-12 ">
           <Hero />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,15 @@ import { FaXTwitter } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
+    <nav className="mb-20 mt-5 flex items-center justify-between py-6">
       <div className=" flex flex-shrink-0 items-center">
         <img
-          className="mx-2 w-10 transition-all duration-300 hover:scale-125"
+          className="ml-2 w-10 transition-all duration-300 hover:scale-125"
           src={logo}
           alt="logo"
         />
       </div>
-      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+      <div className="flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin className="transition-all duration-300 hover:scale-125" />
         <FaGithub className="transition-all duration-300 hover:scale-125" />
         <FaInstagram className="transition-all duration-300 hover:scale-125" />
