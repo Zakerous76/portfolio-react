4c00666ed01d4ed72434e91fe516ccb60675fba6
zakerous <m.z.humayoon@gmail.com>
2025-02-11 21:37:56 +0300

    Experience: done

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,12 +6,26 @@ const Experience = () => {
       <h1 className="my-20 text-center text-4xl">Experience</h1>
       <div>
         {EXPERIENCES.map((exp, ind) => (
-          <div key={ind} className="mb-8 flex flex-wrap lg:justify-center">
+          <div
+            key={ind}
+            className="mb-8 flex flex-wrap lg:justify-center mx-auto"
+          >
             <div className="w-full lg:w-1/4">
-              <p>{exp.year}</p>
+              <p className="mb-2 text-sm text-neutral-400">{exp.year}</p>
             </div>
-            <div className="w-full lg:w-1/4">
-              <p>{exp.description}</p>
+            <div className="w-full max-w-xl lg:3/4">
+              <h6 className="mb-2 font-semibold">
+                {exp.role} -{" "}
+                <span className="text-sm text-purple-100">{exp.company}</span>
+              </h6>
+              <p className="flex mb-4 text-neutral-400">{exp.description}</p>
+              <span className="flex flex-wrap">
+                {exp.technologies.map((tech, ind) => (
+                  <span className="mr-2 mt-4 bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-500">
+                    {tech}
+                  </span>
+                ))}
+              </span>
             </div>
           </div>
         ))}
