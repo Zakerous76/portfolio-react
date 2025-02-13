7f15914ecd94962e3d5b8e876aed5bd27462777e
zakerous <m.z.humayoon@gmail.com>
2025-02-13 09:59:34 +0300

    Hero: Animations done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,51 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
 import profile from "../assets/kevinRushProfile.jpg";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { duration: 0.5, delay: delay } },
+});
+
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              transition={{ duration: 0.5, delay: 0 }}
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+            >
               Zaker Amin
-            </h1>
-            <span className="bg-gradient-to-r from-pink-500 via-slate-500 to-purple-500  bg-clip-text text-4xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span
+              variants={container(0.3)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-500 via-slate-500 to-purple-500  bg-clip-text text-4xl tracking-tight text-transparent"
+            >
               Full Stack Developer
-            </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter text-justify">
+            </motion.span>
+            <motion.p
+              variants={container(0.6)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter text-justify"
+            >
               {HERO_CONTENT}
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8 justify-items-center">
-          <img
+          <motion.img
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.2, delay: 0 }}
             className="rounded-2xl px-2"
             src={profile}
             alt="profile picture"
