eba964f6c559eb4fae505dc36b69911748628f7b
zakerous <m.z.humayoon@gmail.com>
2025-02-14 23:33:29 +0300

    About: animation done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import aboutImg from "../assets/about.jpg";
 import { ABOUT_TEXT } from "../constants";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
@@ -11,16 +12,26 @@ const About = () => {
       </h1>
       <div className="flex flex-wrap">
         {/* Image Container */}
-        <div className="w-full lg:w-1/2 lg:p-8 flex justify-center items-center">
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8 flex justify-center items-center"
+        >
           <img className="rounded-2xl px-2" src={aboutImg} alt="about" />
-        </div>
+        </motion.div>
 
         {/* Text Container */}
-        <div className="w-full lg:w-1/2 lg:flex items-center">
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:flex items-center"
+        >
           <div className="flex justify-center lg:justify-start tracking-tighter text-justify">
             <p className="my-2 max-w-xl py-6 font-light">{ABOUT_TEXT}</p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
